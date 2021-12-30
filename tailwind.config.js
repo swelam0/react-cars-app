ea5978999ea5b1c5fc227ce644f6672dcf4fb62a
m-swelam <mid2o0o11@gmail.com>
2021-12-30 11:23:43 +0200

    tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
+    Screen: {
+      sm: "640px",
+      // @media (min-width: 640px) { ... }
+
+      md: "992px",
+      // @media (min-width: 992px) { ... }
+
+      lg: "1024px",
+      // @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // @media (min-width: 1280px) { ... }
+
+      "2xl": "1563px",
+      // @media (min-width: 1563px) { ... }
+    },
   },
   plugins: [],
 }

433757d9c8d21459da3c2b690b8a66a9b966c4ae
m-swelam <mid2o0o11@gmail.com>
2022-01-01 14:13:25 +0200

    index container styled

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import styled from "styled-components"
+import tw from "twin.macro"
+
+const PageContainer = styled.div`
+  ${tw`flex flex-col w-full h-full items-center overflow-x-hidden`}
+`
+
+export const HomePage = () => {
+  return (
+    <PageContainer>
+      <h1>Home Page</h1>
+    </PageContainer>
+  )
+}
